cbc4daa7d3df5203a38a467122483c22e4a5f773
Kiril Vatev <vatev.1@gmail.com>
2019-04-12 22:49:38 -0400

    cleaning up script, adding notes
    notes from https://github.com/olivierlevon/dcraw/blob/221818e049d13b7a120accf308edf94523f3ab36/build.bat

--- windows/build.bat
+++ windows/build.bat
@@ -1,1 +1,7 @@
-cl /ML /nologo /O2 /Ox /DNODEPS /DDJGPP /DWIN32 ..\src\dcraw.c
+@echo off
+
+cl /nologo /O2 /Ox /DNODEPS /DDJGPP /DWIN32 ..\src\dcraw.c
+
+REM notes
+REM cl /MT /nologo /O2 /Ox -c /arch:SSE2  -D_X86_=1  /D_WINDOWS /D_WIN32_WINDOWS=0x501 /DWINVER=0x501 /D_CRT_SECURE_NO_WARNINGS /D_WIN32 /DWIN32  /I ../lcms/include /I ../libjpeg/jpeg  dcraw_im.c
+REM cl /ML /nologo /O2 /Ox -c /arch:SSE2  -D_X86_=1  /D_WINDOWS /D_WIN32_WINDOWS=0x0601 /DWINVER=0x0601 /D_CRT_SECURE_NO_WARNINGS /D_WIN32 /DWIN32  /I ../lcms/include /I ../libjpeg/jpeg  dcraw_im.c
